f8032be95648fb1d1682efa1f8dc95f196585677
Devesh Gupta <keshav.gupta@hotmail.com>
2024-02-28 01:09:29 +0530

    Restructing

--- app.js
+++ app.js
@@ -1,55 +1,60 @@
-const projectlist=document.querySelector('#prolist');
-const form=document.querySelector('#enterIdea');
+const projectlist = document.querySelector("#prolist");
+const form = document.querySelector("#enterIdea");
 
-function renderplist(doc){
-    let li=document.createElement('li');
-    let pro= document.createElement('span');
-    let desc=document.createElement('pre');
-    let cross=document.createElement('div');
-    let edit=document.createElement('div1');
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyAMrKpjZHQ6Jpoyy9LmneDLmw6-eYfrf2A",
+  authDomain: "minip-17d3c.firebaseapp.com",
+  databaseURL: "https://minip-17d3c.firebaseio.com",
+  projectId: "minip-17d3c",
+  storageBucket: "",
+  messagingSenderId: "279851214475",
+  appId: "1:279851214475:web:afeb23ca4ab74a7f",
+});
+const db = firebaseApp.firestore();
+db.settings({ timestampsInSnapshots: true, merge: true });
 
-    
+function renderplist(doc) {
+  let li = document.createElement("li");
+  let pro = document.createElement("span");
+  let desc = document.createElement("pre");
+  let cross = document.createElement("div");
+  let edit = document.createElement("div1");
 
-    li.setAttribute('data-id',doc.id);
-    pro.textContent = doc.data().pro;
-    desc.textContent=doc.data().desc;
-    cross.textContent = 'x';
-    edit.textContent = "edit";
+  li.setAttribute("data-id", doc.id);
+  pro.textContent = doc.data().pro;
+  desc.textContent = doc.data().desc;
+  cross.textContent = "x";
+  edit.textContent = "edit";
 
-    li.appendChild(pro);
-    li.appendChild(desc);
-    li.appendChild(edit);
-    li.appendChild(cross);
+  li.appendChild(pro);
+  li.appendChild(desc);
+  li.appendChild(edit);
+  li.appendChild(cross);
 
+  projectlist.appendChild(li);
 
-    projectlist.appendChild(li);
+  cross.addEventListener("click", (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute("data-id");
+    db.collection("PLIST").doc(id).delete();
+  });
 
-    cross.addEventListener('click', (e) =>{
-        e.stopPropagation();
-        let id= e.target.parentElement.getAttribute('data-id');
-        db.collection('PLIST').doc(id).delete();
-    })
-
-    edit.addEventListener('click', (e) =>{
-        e.stopPropagation();
-        let id= e.target.parentElement.getAttribute('data-id');
-        var name=prompt("Enter the name of your idea!","");
-        if(name==="")
-        {
-            alert("ENTER IDEA NAME FOR GOD SAKE!");
-            return;
-        }
-        else{
-            var descr=prompt("Enter the description!","");
-            db.collection('PLIST').doc(id).update(
-                {
-                    pro: name,
-                    desc: descr
-                }
-            );
-            alert("Refresh to see changes");
-        }
-    })
+  edit.addEventListener("click", (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute("data-id");
+    var name = prompt("Enter the name of your idea!", "");
+    if (name === "") {
+      alert("ENTER IDEA NAME FOR GOD SAKE!");
+      return;
+    } else {
+      var descr = prompt("Enter the description!", "");
+      db.collection("PLIST").doc(id).update({
+        pro: name,
+        desc: descr,
+      });
+      alert("Refresh to see changes");
+    }
+  });
 }
 
 /* db.collection('PLIST').get().then((snapshot) => {
@@ -58,35 +63,31 @@ function renderplist(doc){
     })
 }) */
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    var empt= document.form1.eidea.value;
-    if(empt==="")
-    {
-        alert("Boo!!! Please enter something atleast!!!");
-        return false;
-
-    }
-    else{
-    db.collection('PLIST').add({
-        pro: form.eidea.value,
-        desc: form.idead.value
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var empt = document.form1.eidea.value;
+  if (empt === "") {
+    alert("Boo!!! Please enter something atleast!!!");
+    return false;
+  } else {
+    db.collection("PLIST").add({
+      pro: form.eidea.value,
+      desc: form.idead.value,
     });
-    form.eidea.value='';
-    form.idead.value='';
+    form.eidea.value = "";
+    form.idead.value = "";
     return true;
-}
+  }
 });
 
-db.collection('PLIST').onSnapshot(snapshot =>{
-    let changes=snapshot.docChanges();
-    changes.forEach(change=>{
-        console.log(change.doc.data());
-        if(change.type == 'added'){
-            renderplist(change.doc);
-        } else if (change.type == 'removed'){
-            let li = projectlist.querySelector('[data-id=' + change.doc.id + ']');
-            projectlist.removeChild(li);
-        }
-    });
+db.collection("PLIST").onSnapshot((snapshot) => {
+  let changes = snapshot.docChanges();
+  changes.forEach((change) => {
+    if (change.type == "added") {
+      renderplist(change.doc);
+    } else if (change.type == "removed") {
+      let li = projectlist.querySelector("[data-id=" + change.doc.id + "]");
+      projectlist.removeChild(li);
+    }
+  });
 });
